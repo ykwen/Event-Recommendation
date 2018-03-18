91c7039efc61fc03b880c413d2e2ce0269872720
Yuekun <yg2519@columbia.edu>
2018-03-18 17:55:20 -0400

    First commit

---
+++ src/db/DBConnectionFactory.java
@@ -0,0 +1,26 @@
+package db;
+
+import db.mongodb.MongoDBConnection;
+import db.mysql.MySQLConnection;
+
+public class DBConnectionFactory {
+    private static final String DEFAULT_DB = "mongodb";
+
+    public static DBConnection getDBConnection(String db) {
+        switch (db) {
+        case "mysql":
+            return new MySQLConnection();
+        case "mongodb":
+            //return MongoDBConnection.getInstance();
+            return new MongoDBConnection();
+        // You may try other dbs and add them here.
+        default:
+            throw new IllegalArgumentException("Invalid db " + db);
+        }
+    }
+
+    public static DBConnection getDBConnection() {
+        return getDBConnection(DEFAULT_DB);
+    }
+}
+
